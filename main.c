bb1762e3cfad7f94f7658097bdd517376419ab22
Onkar Jondhale <143188796+OnkarJondhale@users.noreply.github.com>
2025-09-13 10:34:55 +0530

    Update main.c (Test Build Trigger)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("This C code is getting executed by JENKINS AUTOMATION SERVER");
-    printf("Hello, World!");
+    printf("This C code is getting executed by JENKINS AUTOMATION SERVER \n");
+    printf("Hello, World! \n");
     return 0;
 }
